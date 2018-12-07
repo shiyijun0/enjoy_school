6dc013c90d305ca503b15f500664334f440b84bf
shiyijun0 <617318842@qq.com>
2018-12-07 08:02:29 +0800

    提交高并发3

---
+++ vip-concurrent-3/src/com/xiangxue/ch3/AtomicArray.java
@@ -0,0 +1,17 @@
+package com.xiangxue.ch3;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+
+/**
+ *@author Mark老师   享学课堂 https://enjoy.ke.qq.com 
+ *
+ *类说明：
+ */
+public class AtomicArray {
+    static int[] value = new int[] { 1, 2 };
+    
+    public static void main(String[] args) {
+    	
+    }
+}
