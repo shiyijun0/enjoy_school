dec03bd615a37488b17e330ab8e8d4e52aaf5731
shiyijun <1178078767@qq.com>
2018-12-12 17:51:55 +0800

    提交jvm

--- vip-jvm/src/com/xiangxue/ch01/StackAlloc.java
+++ vip-jvm/src/com/xiangxue/ch01/StackAlloc.java
@@ -1,7 +1,7 @@
 package com.xiangxue.ch01;
 
 public class StackAlloc {
-	
+	private static int age;
 	public static class User{
 		public int id = 0;
 		public String name = "";
@@ -11,17 +11,27 @@ public class StackAlloc {
 		}
 	}
 	
-	public static User alloc() {
+	public  static  User alloc() {
 		User u = new User();
 		u.id = 5;
+		int i=u.id;
 		u.name = "mark";
+		System.out.println(age++);
 		return u;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		long b = System.currentTimeMillis();
-		for(int i=0;i<100000000;i++) {
-			alloc();
+		Thread thread=new Thread(()->alloc());
+		Thread thread1=new Thread(new Runnable() {
+			@Override
+			public void run() {
+				alloc();
+			}
+		});
+		for(int i=0;i<1;i++) {
+			thread1.start();
+
 		}
 		long e = System.currentTimeMillis();
 		System.out.println((e-b)+"ms");
